87f240e1b4fd2cc64102325dac9c70895112d18a
carlos <cgaldea@gmail.com>
2019-03-11 16:12:06 +0000

    subida test y requirementes

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 from  flask   import Flask
 app = Flask(__name__)
 
+
+def suma (a,b):
+	return a + b
+
+
 @app.route("/")
+
 def hello():
-	return "hello world"
+	res = suma (3,2)
+	return "hello world %s" % (res)
 
 
 if __name__ == '__main__' :
